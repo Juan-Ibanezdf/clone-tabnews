e01a65233de3e1a15baee4b5ee75f0eed2efc0b0
Juan Daniel Ferreira Ibanez <105465832+Juan-Ibanezdf@users.noreply.github.com>
2025-06-23 16:56:43 +0000

    add logic CA

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(querryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -25,3 +25,12 @@ async function query(querryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+}
